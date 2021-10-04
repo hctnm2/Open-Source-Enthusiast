ddbb7180090a1c84adc812d82e7513f3851fc8e0
Rohan j <59871907+rohan-cce@users.noreply.github.com>
2021-10-04 12:44:50 +0530

    create Odd Length String Diagonal Pattern.c

---
+++ codes/Odd Length String Diagonal Pattern.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ char str[1000];
+ scanf("%s",str);
+ int len = strlen(str);
+ 
+ for(int i=0;i<len;i++)
+ { 
+ int g=len-i-1;
+ for(int j=0;j<len;j++)
+ { 
+ if(i==j)
+ printf("%c",str[j]);
+ else if(j==g)
+ printf("%c",str[j]);
+ else
+ printf(" ");
+ 
+ }
+ printf("\n");
+ }
+}
