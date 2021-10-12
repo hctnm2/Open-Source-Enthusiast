ec3f6d997e173e7c125ea0bdc9bb1f5334befbc3
B-Anupam <71441129+B-Anupam@users.noreply.github.com>
2021-10-12 20:55:37 +0530

    Create bitwiseoperator.cpp

---
+++ bitwiseoperator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int a,b;
+  cin>>a>>b;
+
+	cout<<"a = " << a <<","<< " b = " << b <<endl;
+	cout << "a & b = " << (a & b) << endl;
+
+
+	cout << "a | b = " << (a | b) << endl;
+
+
+	cout << "a ^ b = " << (a ^ b) << endl;
+
+	cout << "~(" << a << ") = " << (~a) << endl;
+
+
+
+
+	return 0;
+}
+
