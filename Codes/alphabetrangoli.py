5ceded669775d9068413a15c09308a9ab3c44cd1
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-10-17 10:38:23 +0530

    Create alphabetrangoli.py

---
+++ Codes/alphabetrangoli.py
@@ -0,0 +1,11 @@
+import string
+
+def print_rangoli(n):
+    design = string.ascii_lowercase
+    L = []
+    for i in range(n):
+        s = "-".join(design[i:n])
+        L.append((s[::-1]+s[1:]).center(4*n-3, "-"))
+        
+    print('\n'.join(L[:0:-1]+L))
+    
